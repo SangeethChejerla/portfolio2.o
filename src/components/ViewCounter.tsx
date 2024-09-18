6e170198ea4912b08bedd506c740e196ec81a1e8
sangeeth-Chejerla <sangeethreddychejerla@gmail.com>
2024-09-18 18:03:39 +0530

    feat:Edited few things

--- src/components/ViewCounter.tsx
+++ src/components/ViewCounter.tsx
@@ -10,9 +10,5 @@ export default function ViewCounter({ slug, allViews }: ViewCounterProps) {
   const viewsForSlug = allViews.find((view) => view.slug === slug);
   const number = new Number(viewsForSlug?.count || 0);
 
-  return (
-    <p className="text-neutral-600 dark:text-neutral-400">
-      {`${number.toLocaleString()} views`}
-    </p>
-  );
+  return <p className="text-white">{`${number.toLocaleString()} views`}</p>;
 }
